bfe5c6e361b7738e4d84b35af03c1073c6d32d83
lixiaodan04 <lixiaodan04@meituan.com>
2017-10-24 14:54:04 +0800

    monkey

---
+++ monkey.js
@@ -0,0 +1,7 @@
+if(document.location.href.indexOf("homepage.jsp")){
+   $("#jjzbl").click(); 
+}
+
+if(document.location.href.indexOf("jsp/enterbj/index.html)){
+}
+$("<div>hello world</div>").appendTo(document.body)

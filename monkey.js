d8fae73b2660ec713fe93cb48767492593a5dc3e
lixiaodan04 <lixiaodan04@meituan.com>
2018-08-15 15:39:02 +0800

    高德驾乘mock,精简

--- monkey.js
+++
@@ -1,7 +0,0 @@
-if(document.location.href.indexOf("homepage.jsp")){
-   $("#jjzbl").click(); 
-}
-
-if(document.location.href.indexOf("jsp/enterbj/index.html)){
-}
-$("<div>hello world</div>").appendTo(document.body)
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export MM_SMTP=''
-export MM_USERNAME=''
-export MM_PASSWORD=''
-export MM_FROM_ADDR=''
-./miniweb -p 45678
